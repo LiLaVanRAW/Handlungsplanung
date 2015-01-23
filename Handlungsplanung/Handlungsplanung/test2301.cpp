aee6df7beec6c3a2a68fb637ffc41ae2cda79cd7
LiLaVanRAW <Alexmausy1990@freenet.de>
2015-01-23 21:57:40 +0100

    akt2
    describe

--- Handlungsplanung/Handlungsplanung/test2301.cpp
+++ Handlungsplanung/Handlungsplanung/test2301.cpp
@@ -92,6 +92,136 @@ string tablefree(char tableverwenden)
 /*--------------------Anfang----Aktionen-----------------------*/
 /*-------------------------------------------------------------*/
 
+// erstellt den string zur Aktion stack(X,a,Y)
+// enthält die pre/add/del-Listen der Eigenschaften zu dieser Aktion
+string stack(int wert1, char start, int ziel)
+{
+	set<string> pre;
+	pre.insert(table(wert1));
+	pre.insert(table(ziel));
+	pre.insert(clear(wert1));
+	pre.insert(clear(ziel));
+	pre.insert(on(wert1,start));
+
+	set<string> add;
+	add.insert(on(wert1,ziel));
+	add.insert(tablefree(start));
+
+	set<string> del;
+	del.insert(clear(wert1));
+	del.insert(clear(ziel));
+	del.insert(table(wert1));
+	del.insert(on(wert1,start));
+
+	//INSERT Possible Abfrage hier
+
+	string wort = "";
+	wort += "stack(";
+	wort += to_string(wert1);
+	wort += ",";
+	wort += start;
+	wort += ",";
+	wort += to_string(ziel);
+	wort += ")";
+	return wort;
+}
+
+// erstellt den string zur Aktion unstack(X,Y,a)
+// enthält die pre/add/del-Listen der Eigenschaften zu dieser Aktion
+string unstack(int wert1, int start, char ziel)
+{
+	set<string> pre;
+	//pre.insert(clear(wert1));
+	pre.insert(on(wert1,start));
+	pre.insert(tablefree(ziel));
+	pre.insert(table(start));
+
+	set<string> add;
+	add.insert(table(wert1));
+	add.insert(clear(start));
+	add.insert(clear(wert1));
+	add.insert(on(wert1,ziel));
+
+	set<string> del;
+	del.insert(on(wert1,start));
+	del.insert(tablefree(ziel));
+
+	//INSERT Possible Abfrage hier
+
+	string wort = "";
+	wort += "unstack(";
+	wort += to_string(wert1);
+	wort += ",";
+	wort += to_string(start);
+	wort += ",";
+	wort += ziel;
+	wort += ")";
+	return wort;
+}
+
+// erstellt den string zur Aktion moveOben(X,Y,Z)
+// enthält die pre/add/del-Listen der Eigenschaften zu dieser Aktion
+string moveOben(int wert1, int von, int nach)
+{
+	set<string> pre;
+	//pre.insert(clear(wert1));
+	pre.insert(clear(nach));
+	pre.insert(on(wert1,von));
+	pre.insert(table(von));
+	pre.insert(table(nach));
+
+	set<string> del;
+	del.insert(on(wert1,von));
+	del.insert(clear(nach));
+
+	set<string> add;
+	add.insert(clear(von));
+	add.insert(on(wert1,nach));
+	
+	//INSERT Possible Abfrage hier
+
+	string wort = "";
+	wort += "moveOben(";
+	wort += to_string(wert1);
+	wort += ",";
+	wort += to_string(von);
+	wort += ",";
+	wort += to_string(nach);
+	wort += ")";
+	return wort; 
+}
+
+// erstellt den string zur Aktion moveUnten(X,a,b)
+// enthält die pre/add/del-Listen der Eigenschaften zu dieser Aktion
+string moveUnten(int wert1,char start,char ziel)
+{
+	set<string> pre;
+	pre.insert(clear(wert1));
+	pre.insert(table(wert1));
+	pre.insert(tablefree(ziel));
+	pre.insert(on(wert1,start));
+
+	set<string> add;
+	add.insert(tablefree(start));
+	add.insert(on(wert1,ziel));
+
+	set<string> del;
+	del.insert(tablefree(ziel));
+	del.insert(on(wert1,start));
+
+	//INSERT Possible Abfrage hier
+
+	string wort = "";
+	wort += "moveUnten(";
+	wort += to_string(wert1);
+	wort += ",";
+	wort += start;
+	wort += ",";
+	wort += ziel;
+	wort += ")";
+	return wort;
+}
+
 /*-------------------------------------------------------------*/
 /*--------------------Ende------Aktionen-----------------------*/
 /*-------------------------------------------------------------*/
@@ -100,12 +230,87 @@ string tablefree(char tableverwenden)
 /*--------------------Anfang----Methoden-----------------------*/
 /*-------------------------------------------------------------*/
 
-/*-------------------------------------------------------------*/
-/*--------------------Ende------Methoden-----------------------*/
-/*-------------------------------------------------------------*/
+void createStart(int array[])
+{
+	int stein = 1;
+	char platz = 'A';
+	if(array[0] != 0)
+	{
+		startSituation.insert(table(stein));
+		startSituation.insert(clear(stein));
+		startSituation.insert(on(stein,platz));
+		stein++;
+		if(array[1] != 0)
+		{
+			startSituation.erase(clear(stein-1));
+			startSituation.insert(on(stein, stein-1));
+			stein++;
+		}
+	}
+	else
+	{
+		startSituation.insert(tablefree(platz));
+	}
+	platz++;
+	if(array[2] != 0)
+	{
+		startSituation.insert(table(stein));
+		startSituation.insert(clear(stein));
+		startSituation.insert(on(stein,platz));
+		stein++;
 
-// Erstellt die erste Liste der Eigenschaften
-void createSet(int array[])
+		if(array[3] != 0)
+		{
+			startSituation.erase(clear(stein-1));
+			startSituation.insert(on(stein, stein-1));
+			stein++;
+		}
+	}
+	else
+	{
+		startSituation.insert(tablefree(platz));
+	}
+	platz++;
+	if(array[4] != 0)
+	{
+		startSituation.insert(table(stein));
+		startSituation.insert(clear(stein));
+		startSituation.insert(on(stein,platz));
+		stein++;
+
+		if(array[5] != 0)
+		{
+			startSituation.erase(clear(stein-1));
+			startSituation.insert(on(stein, stein-1));
+			stein++;
+		}
+	}
+	else
+	{
+		startSituation.insert(tablefree(platz));
+	}
+	platz++;
+	if(array[6] != 0)
+	{
+		startSituation.insert(table(stein));
+		startSituation.insert(clear(stein));
+		startSituation.insert(on(stein,platz));
+		stein++;
+
+		if(array[7] != 0)
+		{
+			startSituation.erase(clear(stein-1));
+			startSituation.insert(on(stein, stein-1));
+			stein++;
+		}
+	}
+	else
+	{
+		startSituation.insert(tablefree(platz));
+	}
+}
+
+void createZiel(int array[])
 {
 	int stein = 1;
 	char platz = 'A';
@@ -119,7 +324,6 @@ void createSet(int array[])
 		{
 			aktuelleEigenschaften.erase(clear(stein-1));
 			aktuelleEigenschaften.insert(on(stein, stein-1));
-			//aktuelleEigenschaften.insert(clear(stein));
 			stein++;
 		}
 	}
@@ -139,7 +343,6 @@ void createSet(int array[])
 		{
 			aktuelleEigenschaften.erase(clear(stein-1));
 			aktuelleEigenschaften.insert(on(stein, stein-1));
-			//aktuelleEigenschaften.insert(clear(stein));
 			stein++;
 		}
 	}
@@ -159,7 +362,6 @@ void createSet(int array[])
 		{
 			aktuelleEigenschaften.erase(clear(stein-1));
 			aktuelleEigenschaften.insert(on(stein, stein-1));
-			//aktuelleEigenschaften.insert(clear(stein));
 			stein++;
 		}
 	}
@@ -179,7 +381,6 @@ void createSet(int array[])
 		{
 			aktuelleEigenschaften.erase(clear(stein-1));
 			aktuelleEigenschaften.insert(on(stein, stein-1));
-			//aktuelleEigenschaften.insert(clear(stein));
 			stein++;
 		}
 	}
@@ -189,7 +390,15 @@ void createSet(int array[])
 	}
 }
 
-void ausgabeSet()
+// Erstellt eine Liste der Eigenschaften
+// für Start- / ZielSituation
+void createSet(int a[], int b[])
+{
+	createStart(a);
+	createZiel(b);
+}
+
+void ausgabeAktEig()
 {
 	// Ausgabe der aktuellen Liste der Eigenschaften
 	set<string>::iterator element;
@@ -200,15 +409,35 @@ void ausgabeSet()
 	}
 }
 
+void ausgabeStartEig()
+{
+	// Ausgabe der aktuellen Liste der Eigenschaften
+	set<string>::iterator element;
+	for(element = startSituation.begin(); element != startSituation.end(); element++)
+	{
+		cout << *element;
+		cout << "\n";
+	}
+}
+
+/*-------------------------------------------------------------*/
+/*--------------------Ende------Methoden-----------------------*/
+/*-------------------------------------------------------------*/
+
+/*-------------------------------------------------------------*/
 /*---------------------MAIN-METHODE----------------------------*/
+/*-------------------------------------------------------------*/
 int main(int argc, char* argv)
 {
 	int eingabeStart[8] = {1,1,0,0,1,0,0,0};
 	int eingabeZiel[8] = {1,0,0,0,1,0,1,0};
 
-	createSet(eingabeZiel);
+	createSet(eingabeStart, eingabeZiel);
 
-	ausgabeSet();
+	// Ausgabe
+	ausgabeAktEig();
+	printf("\n------------------\n");
+	ausgabeStartEig();
 	
 
 
